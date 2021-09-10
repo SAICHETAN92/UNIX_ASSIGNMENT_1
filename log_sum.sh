8cde26a08cdcc4eca7bd2e8fe794318cb92d5a9d
Sai Chetan Poluri <54228270+SAICHETAN92@users.noreply.github.com>
2021-09-10 22:29:17 +0200

    Check the successful connections made by IP
    Sorts the IP addresses that made a successful connection attempts

--- log_sum.sh
+++ log_sum.sh
@@ -26,6 +26,23 @@ echo "$ip" | head -n $2
 
 }
 
+log_2()
+{
+#Variable ipc contains all the data from the thttpd.log.
+ip_d=$(<$1)
+
+#it is converted into expression and grep is used for pattern matching and finds number of successful connections.
+su_ip=$(echo "$ip_d" | grep "HTTP\/[1]\.[0-1]\" [2][0-9][0-9]" | grep -Eo '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
+
+#uniq is used to count the occurances of ip address and sorted them in descending order. 
+ip="`echo "$su_ip" | sort | uniq -c | sort -n -r`"
+
+#now the ip address and their successful attempts  are printed
+ip="`echo "$ip" | awk '{print "-2:  " $2"  "$1 " "}'`"
+echo "$ip" | head -n $l
+
+}
+
 maxl=2
 
 
@@ -42,6 +59,9 @@ else
 	case $1 in
 		-c)
 			log_c $2 $l
+			;;
+		-2)
+			log_2 $2
 			;;
 		-?)
 			echo "Please give a command"
