bc6851ec620b21e79b41d251f9c67f4b3c928ffb
Sai Chetan Poluri <54228270+SAICHETAN92@users.noreply.github.com>
2021-09-11 16:35:03 +0200

    Change in Variable names
    Initially considered variables are in short form and it is quite tough to understand them, so the names of variables are changed.

--- log_sum.sh
+++ log_sum.sh
@@ -10,61 +10,60 @@ l=0
 log_c()
 {
 
-#Variable ip_d contains all the data from the thttpd.log.
-ip_d=$(<$1)
+#Variable IP_Data contains all the data from the thttpd.log.
+IP_Data=$(<$1)
 
-#it is converted into expression and grep is used for pattern matching.
-ip=$(echo $ip_d |grep -Eo '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}') 
+#It is converted into expression and grep is used for pattern matching.
+IP_Addr=$(echo $IP_Data |grep -Eo '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}') 
 
 #uniq is used to count the occurances of ip address and sorted them in descending order. 
-ip="`echo "$ip" | sort | uniq -c | sort -n -r`" 
+IP_Addr="`echo "$IP_Addr" | sort | uniq -c | sort -n -r`" 
 
-#now the ip address and their occurance are printed
-ip="`echo "$ip" |awk '{print "-c:  " $2"  "$1 " "}'`"
-echo "$ip" | head -n $2
-
+#Now the ip address and their occurance are printed
+IP_Addr="`echo "$IP_Addr" |awk '{print "-c:  " $2"  "$1 " where "$1" is the number of connection attempts "}'`"
+echo "$IP_Addr" | head -n $2
 
 }
 
 log_2()
 {
-#Variable ip_d contains all the data from the thttpd.log.
-ip_d=$(<$1)
+#Variable IP_Data contains all the data from the thttpd.log.
+IP_Data=$(<$1)
 
-#it is converted into expression and grep is used for pattern matching and finds number of successful connections.
-su_ip=$(echo "$ip_d" | grep "HTTP\/[1]\.[0-1]\" [2][0-9][0-9]" | grep -Eo '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
+#It is converted into expression and grep is used for pattern matching and finds number of successful connections.
+IP_Success=$(echo "$IP_Data" | grep "HTTP\/[1]\.[0-1]\" [2][0-9][0-9]" | grep -Eo '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
 
 #uniq is used to count the occurances of ip address and sorted them in descending order. 
-ip="`echo "$su_ip" | sort | uniq -c | sort -n -r`"
+IP_Addr="`echo "$IP_Success" | sort | uniq -c | sort -n -r`"
 
-#now the ip address and their successful attempts  are printed
-ip="`echo "$ip" | awk '{print "-2:  " $2"  "$1 " "}'`"
-echo "$ip" | head -n $1
+#Now the ip address and their successful attempts  are printed
+IP_Addr="`echo "$IP_Addr" | awk '{print "-2:  " $2"  "$1 " where "$1" is the number of  successful attempts"}'`"
+echo "$IP_Addr" | head -n $l
 
 }
 
 log_r()
 {
-#Variable ip_d contains all the data from the thttpd.log.
-ip_d=$(<$1)
+#Variable IP_Data contains all the data from the thttpd.log.
+IP_Data=$(<$1)
 
-#Argument 9 indicates status code, ip address are sorted as per status code
-st_cod="`echo "$ip_d"| awk '{print $9}' | sort |uniq -c| sort -n -r`"
+#Argument 9 indicates status code, IP address are sorted as per status code
+Result_code="`echo "$IP_Data"| awk '{print $9}' | sort |uniq -c| sort -n -r`"
 
-# status codes are matched using grep command
-st_cod=$(echo "$st_cod"| grep "[2-4][0][0-4]" | grep -Eo "[2-4][0][0-4]")
+# Result codes are matched using grep command
+Result_code=$(echo "$Result_code"| grep "[2-4][0][0-4]" | grep -Eo "[2-4][0][0-4]")
 
-#for loop is used to print all ip addresses under specific status code
-      	for code in $st_cod;
+#for loop is used to print all ip addresses under specific Result code
+      	for code in $Result_code;
 	do
                 #printing status code
 		echo $code
-                
-		#match all the ip addresses with specific status code
-		s_ip=$(echo "$ipc" | grep "HTTP\/[1]\.[0-1]\" $code" | awk '{print $1,$9}')
-                
-		#match all the ip addresses with specific status code
-	        result="`echo "$s_ip"| sort | uniq -c | sort -nr |awk '{print "-r : "$3"  "$2 " "}'`"
+		
+                #match all the ip addresses with specific status code
+		IP_Success=$(echo "$IP_Data" | grep "HTTP\/[1]\.[0-1]\" $code" | awk '{print $1,$9}')
+		
+                #match all the ip addresses with specific status code
+	        result="`echo "$IP_Success"| sort | uniq -c | sort -nr |awk '{print "-r : "$3"  "$2 "where "$3" is the result code "}'`"
                 echo "$result" | head -n $l 
 		
 	done
@@ -73,54 +72,54 @@ st_cod=$(echo "$st_cod"| grep "[2-4][0][0-4]" | grep -Eo "[2-4][0][0-4]")
 
 log_F()
 {
-
-#Variable ip_d contains all the data from the thttpd.log.
-ip_d=$(<$1)
+#Variable IP_Data contains all the data from the thttpd.log.
+IP_Data=$(<$1)
 
 #matching the status code with connection
-st_cod=$(echo "$ipc"|grep  "HTTP\/[1]\.[0-1]\" [4][0][0-4]")
+Result_code=$(echo "$IP_Data"|grep  "HTTP\/[1]\.[0-1]\" [4][0][0-4]")
 
-#storing the values of status code 
-st_cod=$(echo "$status_code" | awk '{print $9}')
+#storing the vals of Result code 
+Result_code=$(echo "$Result_code" | awk '{print $9}')
 
 #sorting the staus codes
-st_cod="`echo "$status_code" | sort| uniq -c| sort -nr | awk '{print $2}'`"
+Result_code="`echo "$Result_code" | sort| uniq -c| sort -nr | awk '{print $2}'`"
 
 #for loop is used to print the status code under the specific status code
-	for code in $status_code;
+	for code in $Result_code;
 	do
-                #printing status code
+                #printing Result code
 		echo $code
-               
-	       #match all the ip addresses with specific status code
-		s_ip=$(echo "$ipc" | grep "HTTP\/[1]\.[0-1]\" $code" | awk '{print $1,$9}')
-               
-	       #match all the ip addresses with specific status code
-		result=$(echo "$s_ip"| sort|uniq -c | sort -n -r| awk '{print "-r : "$3"  " $2 " "}')
-		echo "$result" | head -n $l
 		
+                #match all the ip addresses with specific Result code
+		IP_Success=$(echo "$IP_Data" | grep "HTTP\/[1]\.[0-1]\" $code" | awk '{print $1,$9}')
+		
+                #match all the ip addresses with specific status code
+		result=$(echo "$IP_Success"| sort|uniq -c | sort -n -r| awk '{print "-r : "$3"  " $2 " where " $3" is the result code indicating failure  "}')
+		echo "$result" | head -n $l
+
 	done		        
 }
 
+
 log_t()
 {
-
 #Variable IP_Data contains all the data from the thttpd.log.
 IP_Data=$(<$1)
 
 #it captures the ip address along with the number of bytes tranmitted
 store_IPByte=$(echo "$IP_Data" | awk '{print $1,$10}'| sort -k1,1)
-#echo "$store_IPByte"
 
+#echo "$store_IPByte"
 #it removes all failed connections
 store_IPByte="`echo "$store_IPByte" |awk '{print $1,$2}'|grep -v "-"`"
-#echo "$store_IPByte"
 
+#echo "$store_IPByte"
 #it sort every ip address in decresing order
 store_IP=$(echo "$store_IPByte" |awk '{print $1}'| sort | uniq |sort -nr| awk '{print $1}')
 #echo "$store_IP"
 
-	declare -A Lst_IP
+declare -A Lst_IP
+
 	#for loop is used to add up all the byte transmitations under similar ip address.
 	for IP in ${store_IP[@]};
 	do
@@ -134,12 +133,11 @@ store_IP=$(echo "$store_IPByte" |awk '{print $1}'| sort | uniq |sort -nr| awk '{
 		Lst_IP[$IP]=$sum 
 	done	
 	
-                # this for loop is used to print the ip address along with the number of byte transmissions.   
-		for IP in ${!Lst_IP[@]}; 
-		do
-			echo -e "-t:$IP   ${Lst_IP[$IP]} where ${Lst_IP[$IP]} is the number of bytes sent from the server"
-		done | sort -k2 -r -n | head -n $l	
-	 
+        # this for loop is used to print the ip address along with the number of byte transmissions.   
+	for IP in ${!Lst_IP[@]}; 
+	do
+		echo -e "-t:$IP   ${Lst_IP[$IP]} where ${Lst_IP[$IP]} is the number of bytes sent from the server"
+	done | sort -k2 -r -n | head -n $l	 
 
 }
 
@@ -158,8 +156,7 @@ then
 	echo "Maximum number of arguemnts is reached"
 else
 	case $1 in
-		-c)
-			
+		-c)			
 			log_c $2 $l
 			;;
 		-2)
@@ -176,7 +173,7 @@ else
 			;;
 
 		-?)
-			echo "U should give me a command"
+			echo "Please give a command"
 			;;
 	esac
 fi	
